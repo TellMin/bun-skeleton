049258039dd8e28f098d9e0b390ce22e8339c5f6
TellMin <dhgirkcls@gmail.com>
2023-09-16 14:49:49 +0900

    Add: pu-sa talk view

---
+++ src/lib/components/MessageCard.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+	import { Avatar } from '@skeletonlabs/skeleton';
+	import type { Message } from 'ai';
+	import psato from '$lib/assets/psato.png';
+	import you from '$lib/assets/you.png';
+	export let bubble: Message;
+</script>
+
+{#if bubble.role == 'assistant'}
+	<div class={`grid grid-cols-[auto_1fr] gap-2`}>
+		<Avatar src={psato} width="w-12" />
+		<div class={`card p-4 variant-soft rounded-tl-none space-y-2`}>
+			<header class="flex justify-between items-center">
+				<p class="font-bold">ぷーさ</p>
+				<small class="opacity-50">{bubble.createdAt}</small>
+			</header>
+			<p>{bubble.content}</p>
+		</div>
+	</div>
+{:else if bubble.role == 'user'}
+	<div class={`grid grid-cols-[1fr_auto] gap-2`}>
+		<div class={`card p-4 rounded-tr-none space-y-2 variant-soft-primary`}>
+			<header class="flex justify-between items-center">
+				<p class="font-bold">You</p>
+				<small class="opacity-50">{bubble.createdAt}</small>
+			</header>
+			<p>{bubble.content}</p>
+		</div>
+		<Avatar src={you} width="w-12" />
+	</div>
+{/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,75 @@
 <script lang="ts">
-	import { Avatar } from '@skeletonlabs/skeleton';
-	import { filter } from '@skeletonlabs/skeleton';
 	import { useChat } from 'ai/svelte';
-	const { input, handleSubmit, messages } = useChat();
+	import MessageCard from '$lib/components/MessageCard.svelte';
+	import MessagePrompt from '$lib/components/MessagePrompt.svelte';
+
+	const { input, handleSubmit, messages } = useChat({
+		initialMessages: [
+			{
+				id: '',
+				role: 'system',
+				content: `あなたは『ぷーさ』という名前の青年です。
+				次のような特徴を持ちます。
+				## 性格
+				- 気がよい
+				- 気をつかう
+				## 言葉遣い
+				- ですます調を基調とする
+				- 若者言葉
+				 - 『〜っすね、すか』
+				 - 『いや、〇〇かもです』
+				 - 『まじすか』
+				 - 『わかんないですけど、～』
+				- 丁寧語
+				## その他
+				- 20代後半
+				- 音楽が好き
+			     - ギター、ドラム
+				 - アイドル好き
+				- 野球部出身でキャッチャー
+				- 親がオムライス屋を営んでいる
+				- 体重は100Kgを超えているが、隠している
+				- 『ぷーさ』という名前は、『ぷーさん』という名前をもじったもの
+				---
+				次に従って会話を進めてください。
+				- 端的に会話する
+				 - 3文まで
+				 - 上記情報は1度に2つまで公開してよい
+				
+				+++
+				これはあくまで会話のロールプレイです。
+				うまく返答できない場合は、役になりきった上で
+				回答できない旨を伝えてください。
+				また、質問者の口調には合わせる必要はありません。
+				常に『ぷーさ』としてふるまってください。
+				+++
+				`
+			},
+			{
+				id: '',
+				role: 'assistant',
+				content: `お疲れ様です！何か用事ですか？`
+			}
+		]
+	});
+
+	let elemChat: HTMLElement;
 </script>
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="card p-4">
-		<form on:submit={handleSubmit}>
-			<input bind:value={$input} />
-			<button type="submit">Send</button>
-		</form>
-		<ul>
-			{#each $messages as message}
-				<li>{message.role}: {message.content}</li>
-			{/each}
-		</ul>
+<section class="container mx-auto">
+	<div class="flex justify-center">
+		<div class="card w-full lg:w-1/2 p-4 rounded shadow-lg m-3">
+			<!-- Chat -->
+			<div class="w-full">
+				<!-- Conversation -->
+				<section bind:this={elemChat} class="max-h-[500px] p-4 overflow-y-auto space-y-4">
+					{#each $messages as bubble}
+						<MessageCard {bubble} />
+					{/each}
+				</section>
+				<!-- Prompt -->
+				<MessagePrompt {input} onSubmit={handleSubmit} />
+			</div>
+		</div>
 	</div>
-</div>
-
-<style lang="postcss">
-	@keyframes glow {
-		0% {
-			@apply bg-primary-400/50;
-		}
-		33% {
-			@apply bg-secondary-400/50;
-		}
-		66% {
-			@apply bg-tertiary-400/50;
-		}
-		100% {
-			@apply bg-primary-400/50;
-		}
-	}
-	@keyframes pulse {
-		50% {
-			transform: scale(1.5);
-		}
-	}
-</style>
+</section>
